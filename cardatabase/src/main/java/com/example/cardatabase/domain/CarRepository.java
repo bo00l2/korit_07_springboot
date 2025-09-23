0148f58b8c353b05dc2e812b6a6e6f44e7e3fab8
bo00l2 <kby2333@naver.com>
2025-09-23 11:05:34 +0900

    feat: h2 인메모리 설정 관련 코드

--- cardatabase/src/main/java/com/example/cardatabase/domain/CarRepository.java
+++ cardatabase/src/main/java/com/example/cardatabase/domain/CarRepository.java
@@ -1,4 +1,7 @@
 package com.example.cardatabase.domain;
 
-public interface CarRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CarRepository extends JpaRepository<Car, Long> {
+
 }

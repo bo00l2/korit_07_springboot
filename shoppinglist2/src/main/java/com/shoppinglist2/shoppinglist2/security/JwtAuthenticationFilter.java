13166cd7f2fe16608152a442911fcb78633c6eb5
bo00l2 <kby2333@naver.com>
2025-10-28 17:44:51 +0900

    feat: 20251028 Google OAuth2 Backend

--- shoppinglist2/src/main/java/com/shoppinglist2/shoppinglist2/security/JwtAuthenticationFilter.java
+++ shoppinglist2/src/main/java/com/shoppinglist2/shoppinglist2/security/JwtAuthenticationFilter.java
@@ -17,6 +17,7 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
+
     private final JwtService jwtService;
     private final UserDetailsServiceImpl userDetailsService;
 
